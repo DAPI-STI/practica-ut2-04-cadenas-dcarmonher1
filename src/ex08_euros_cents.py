ad7599839120981db4e8ed03152656ed2f5cb000
David Carmona Herrera <dcarmonher1@educacion.navarra.es>
2025-12-17 18:50:15 +0100

    Ej 8 terminado

--- src/ex08_euros_cents.py
+++ src/ex08_euros_cents.py
@@ -13,4 +13,19 @@ Valida que haya exactamente dos decimales; en caso contrario, ValueError.
 def euros_cents(price_str: str) -> tuple[int, int]:
     """Separa la parte entera (euros) y los céntimos a partir de una cadena."""
     # TODO: sustituye coma por punto, separa, valida y convierte a enteros
-    raise NotImplementedError("Implementa euros_cents(price_str)")
+    precio = price_str.replace(",",".").strip()
+    partes = precio.split(".")
+
+    if len(partes) != 2:
+        raise ValueError("El precio no consta de dos decimales")
+    
+    euros , centimos = partes
+
+    if len(centimos) != 2 or not euros.isdigit() or not centimos.isdigit():
+        raise ValueError("Debe contener exactamente dos decimales numéricos")
+
+    euros = int(euros)
+    centimos = int(centimos)
+    return (euros, centimos)
+    
+
