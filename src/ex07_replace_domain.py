e27cace65e6014d735d3ec6cdcf663edc6d78b69
David Carmona Herrera <dcarmonher1@educacion.navarra.es>
2025-12-17 19:03:05 +0100

    Ej 9 terminado

--- src/ex07_replace_domain.py
+++ src/ex07_replace_domain.py
@@ -13,4 +13,14 @@ Si el correo no tiene exactamente una arroba, lanza ValueError.
 def replace_domain(email: str, new_domain: str = "ceu.es") -> str:
     """Devuelve el correo con el dominio sustituido por new_domain."""
     # TODO: separa con split("@"), valida y construye la nueva dirección
-    raise NotImplementedError("Implementa replace_domain(email, new_domain)")
+
+    correo = email.strip().split("@")
+
+    if len(correo) != 2:
+        raise ValueError("El correo debe tener una @")
+    
+    correo_electronico, _ = correo
+
+    nuevo_correo = f"{correo_electronico}@{new_domain}"
+
+    return nuevo_correo
