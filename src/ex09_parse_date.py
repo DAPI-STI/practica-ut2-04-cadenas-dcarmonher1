9d8493116d3420efb56193b38231d0eb13c541c5
David Carmona Herrera <dcarmonher1@educacion.navarra.es>
2025-12-18 22:03:19 +0100

    Ejs 9, 10 y 11 terminados

--- src/ex09_parse_date.py
+++ src/ex09_parse_date.py
@@ -14,4 +14,24 @@ Si el formato o los rangos son incorrectos, lanza ValueError.
 def parse_date(date_str: str) -> tuple[int, int, int]:
     """Devuelve (día, mes, año) como enteros a partir de una cadena d/m/aaaa."""
     # TODO: usa split("/"), convierte a int y valida rangos sencillos
-    raise NotImplementedError("Implementa parse_date(date_str)")
+    fecha = date_str.strip().split("/")
+
+    if len (fecha) != 3:
+        raise ValueError("La fecha debe de estar entre /")
+    
+    dia, mes, año = fecha
+
+    dia = int(dia)
+    mes = int(mes)
+    año = int(año)
+    
+    if not (1 <= dia <= 31):
+        raise ValueError("El día debe estar entre 1 y 31")
+    
+    if not (1 <= mes <= 12):
+        raise ValueError("El mes debe estar entre 1 y 12")
+    
+    if not (1 <= año <= 9999):
+        raise ValueError("El año debe estar entre 1 y 9999")
+
+    return (dia, mes, año)
